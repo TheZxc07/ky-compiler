a738c42120caf3af1530a695985859d88644da49
Amr Elhossan <thehorse2003@gmail.com>
2025-05-04 01:17:49 -0400

    added support for divison and multiplication in sap

---
+++ out.as
@@ -0,0 +1,22 @@
+	.globl _start
+	.text
+
+_start:
+	MOVQ $3, %rbx
+	MOVQ $4, %r10
+	ADDQ %r10, %rbx
+	MOVQ $5, %r10
+	MOVQ $2, %r11
+	ADDQ %r11, %r10
+	MOVQ $7, %r11
+	MOVQ $2, %r12
+	ADDQ %r12, %r11
+	MOVQ %r10, %rax
+	IMUL %r11
+	MOVQ %rax, %r11
+	MOVQ $9, %r10
+	MOVQ %r11, %rax
+	CQO
+	IDIVQ %r10
+	MOVQ %rax, %r10
+	SUBQ %r10, %rbx
--- src/sap_codegen.c
+++ src/sap_codegen.c
@@ -16,26 +16,48 @@ void reg_free(reg_t reg){
     reg_alloc_map[reg] = 0;
 }
 
-void expr_codegen(expr* e){
+void expr_codegen(expr* ast){
+
+    FILE* fp = fopen("out.as", "w");
+    fprintf(fp, "\t.globl _start\n\t.text\n\n_start:\n");
+    __expr_codegen(ast, fp);
+}
+
+void __expr_codegen(expr* e, FILE* fp){
     if (e == NULL){
         return;
     }
 
-    expr_codegen(e->left);
-    expr_codegen(e->right);
+    __expr_codegen(e->left, fp);
+    __expr_codegen(e->right, fp);
 
     switch(e->kind){
         case EXPR_VALUE:
             e->reg = reg_alloc();
-            printf("MOVQ %%%d, %s\n", e->value.i, REG_STR_MAP[e->reg]);
+            fprintf(fp, "\tMOVQ $%d, %s\n", e->value.i, REG_STR_MAP[e->reg]);
             break;
         case EXPR_ADD:
-            printf("ADDQ %s, %s\n", REG_STR_MAP[e->left->reg], REG_STR_MAP[e->right->reg]);
+            fprintf(fp, "\tADDQ %s, %s\n", REG_STR_MAP[e->right->reg], REG_STR_MAP[e->left->reg]);
+            reg_free(e->right->reg);
+            e->reg = e->left->reg;
+            break;
+        case EXPR_SUBTRACT:
+            fprintf(fp, "\tSUBQ %s, %s\n", REG_STR_MAP[e->right->reg], REG_STR_MAP[e->left->reg]);
+            reg_free(e->right->reg);
+            e->reg = e->left->reg;
+            break;
+        case EXPR_MULTIPLY:
+            fprintf(fp, "\tMOVQ %s, %%rax\n", REG_STR_MAP[e->left->reg]);
+            fprintf(fp, "\tIMUL %s\n", REG_STR_MAP[e->right->reg]);
+            fprintf(fp, "\tMOVQ %%rax, %s\n", REG_STR_MAP[e->right->reg]);
             reg_free(e->left->reg);
             e->reg = e->right->reg;
             break;
-        case EXPR_SUBTRACT:
-            printf("SUBQ %s, %s\n", REG_STR_MAP[e->left->reg], REG_STR_MAP[e->right->reg]);
+        case EXPR_DIVIDE:
+            fprintf(fp, "\tMOVQ %s, %%rax\n", REG_STR_MAP[e->left->reg]);
+            fprintf(fp, "\tCQO\n");
+            fprintf(fp, "\tIDIVQ %s\n", REG_STR_MAP[e->right->reg]);
+            fprintf(fp, "\tMOVQ %%rax, %s\n", REG_STR_MAP[e->right->reg]);
             reg_free(e->left->reg);
             e->reg = e->right->reg;
             break;
